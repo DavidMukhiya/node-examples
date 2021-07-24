3fc6d7a18a9ac1b2b75bb44f0fa1b822c6d0b783
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2021-07-24 09:50:14 -0700

    node-examples

---
+++ app.js
@@ -0,0 +1,21 @@
+const rectangle = require('./rectangle');
+const rect = require('./rectangle');
+
+function solveRect(l, w) {
+    console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
+
+    rect(l, w, (err, rectangle) => {
+        if (err) {
+            console.log('ERROR:', err.message);
+        } else {
+        console.log(`Area of rectangle with dimensions ${1}, ${w} is: ${rectangle.area()}`);
+        console.log(`perimeter of rectangle with dimensions ${1}, ${w} is: ${rectangle.perimeter()}`);
+        }
+    });
+    console.log('This statement is logged after the call to rect()');
+}
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(5, -3);
